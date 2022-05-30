028a3cfb23c24d164d446c4fdfc2975d20dae644
userharis <mhkcodes@gmail.com>
2022-05-30 22:05:28 +0500

    add work section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 import Hero from "../components/home/hero";
+import WorkSection from "../components/home/work";
 
 const IndexPage = () => {
   return (
     <Layout blogLayout={false}>
       <Hero />
+      <WorkSection />
     </Layout>
   );
 };
