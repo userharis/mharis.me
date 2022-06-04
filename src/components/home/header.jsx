9f08f51a66c6ffe96937c723997a41520ac4501f
userharis <mhkcodes@gmail.com>
2022-06-05 01:08:16 +0500

    add Logo, email

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -1,9 +1,15 @@
 import Container from "./container";
+import Logo from "./logo";
 
 const Header = () => {
   return (
     <header>
-      <Container maxWidth="1200px">Header</Container>
+      <Container maxWidth="1200px">
+        <Logo />
+        <div>
+          <a href="mailto:mail@mharis.me">mail@mharis.me</a>
+        </div>
+      </Container>
     </header>
   );
 };
