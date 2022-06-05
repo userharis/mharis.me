06e5136b67af31531e53795a118fbd6613fc9109
userharis <mhkcodes@gmail.com>
2022-06-06 02:20:12 +0500

    add styles for Hero

--- src/components/home/hero/hero.jsx
+++ src/components/home/hero/hero.jsx
@@ -1,18 +1,21 @@
 import Button from "../button";
+import * as styles from "../../../styles/home/home.module.css";
+
+const { hero, heroTitle, heroPara, btnContainer } = styles;
 
 const Hero = () => {
   return (
-    <section>
+    <section className={hero}>
       <div>
-        <h1>
+        <h1 className={heroTitle}>
           Hey, <br />I build Websites
         </h1>
-        <p>
+        <p className={heroPara}>
           I am Haris, a freelance web developer. I build search engine friendly
           websites that load in milliseconds (instead of seconds), and perform
           best on different screen sizes and browsers.{" "}
         </p>
-        <div>
+        <div className={btnContainer}>
           <Button to="/#contact">Get a Quote</Button>
           <Button to="/#featured">See Recent Works</Button>
         </div>
