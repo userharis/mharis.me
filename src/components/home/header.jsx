dd76ef3e1136147a112e0ff761df138560182a19
userharis <mhkcodes@gmail.com>
2022-06-05 21:12:38 +0500

    add styles for header

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -1,12 +1,15 @@
 import Container from "./container";
 import Logo from "./logo";
+import * as styles from "../../styles/home/home.module.css";
+
+const { header, email } = styles;
 
 const Header = () => {
   return (
-    <header>
+    <header className={header}>
       <Container maxWidth="1200px">
         <Logo />
-        <div>
+        <div className={email}>
           <a href="mailto:mail@mharis.me">mail@mharis.me</a>
         </div>
       </Container>
