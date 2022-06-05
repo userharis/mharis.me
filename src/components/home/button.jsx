41dedf1d4f1184a2f019ac81bccb9276a0189dda
userharis <mhkcodes@gmail.com>
2022-06-06 01:41:19 +0500

    add styles for Button

--- src/components/home/button.jsx
+++ src/components/home/button.jsx
@@ -1,13 +1,30 @@
 import { Link } from "gatsby";
+import * as styles from "../../styles/home/home.module.css";
 
-const Button = ({ href, to, children, type = "button" }) => {
+const { button } = styles;
+
+const Button = ({ href, to, children, type = "button", className }) => {
+  className = `${button}`;
+
   if (href) {
-    return <a href={href}>{children}</a>;
+    return (
+      <a className={className} href={href}>
+        {children}
+      </a>
+    );
   }
   if (to) {
-    return <Link to={to}>{children}</Link>;
+    return (
+      <Link className={className} to={to}>
+        {children}
+      </Link>
+    );
   }
-  return <button type={type}>{children}</button>;
+  return (
+    <button className={className} type={type}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
