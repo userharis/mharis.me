8a9de5a064e2d1d751e051d609fae253f2638f1e
userharis <mhkcodes@gmail.com>
2022-06-06 23:25:18 +0500

    create component: Blog Section

---
+++ src/components/home/blog/blog.jsx
@@ -0,0 +1,62 @@
+const { useStaticQuery, graphql, Link } = require("gatsby");
+const { default: SectionLabel } = require("../sectionLabel");
+
+const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query BlogQuery {
+      allMarkdownRemark(
+        sort: { fields: frontmatter___updatedAt, order: DESC }
+        limit: 3
+      ) {
+        nodes {
+          frontmatter {
+            summary
+            title
+            updatedAt
+          }
+          fileAbsolutePath
+          timeToRead
+        }
+      }
+    }
+  `);
+
+  return (
+    <section>
+      <SectionLabel>Latest Articles</SectionLabel>
+      {data.allMarkdownRemark.nodes.map((node, i) => {
+        let { title, summary, updatedAt } = node.frontmatter;
+        updatedAt = formatDate(updatedAt);
+        return (
+          <article>
+            <div>
+              <span>{updatedAt}</span>
+              <span>{node.timeToRead} mins Read</span>
+            </div>
+            <h3>{title}</h3>
+            <p>{summary}</p>
+            <Link to={getSlug(node.fileAbsolutePath)}>Read More</Link>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+
+function formatDate(date) {
+  date = new Date(date);
+  const FormatObject = new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  date = FormatObject.format(date);
+  return date;
+}
+
+function getSlug(fileAbsolutePath) {
+  return "/maniac/" + fileAbsolutePath.split("/").pop().split(".")[0];
+}
+
+export default Blog;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import Layout from "../components/home/layout/layout";
 import Hero from "../components/home/hero/hero";
 import Featured from "../components/home/featured/featured";
+import Blog from "../components/home/blog/blog";
 
 const IndexPage = () => {
   return (
     <Layout>
       <Hero />
       <Featured />
+      <Blog />
     </Layout>
   );
 };
