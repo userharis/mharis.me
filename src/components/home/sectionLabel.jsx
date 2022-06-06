27e1b902bdac68afc5b9d305af14c2655aa65726
userharis <mhkcodes@gmail.com>
2022-06-06 17:07:52 +0500

    add styles

--- src/components/home/sectionLabel.jsx
+++ src/components/home/sectionLabel.jsx
@@ -1,5 +1,7 @@
+import * as styles from "../../styles/home/home.module.css";
+
 const SectionLabel = ({ children }) => {
-  return <h2>{children}</h2>;
+  return <h2 className={styles.sectionLabel}>{children}</h2>;
 };
 
 export default SectionLabel;
