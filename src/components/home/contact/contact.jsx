815bca474e1f4bb2cf0b56d4c248871fcd24a58e
userharis <mhkcodes@gmail.com>
2022-06-09 00:18:46 +0500

    add styles for contact section

--- src/components/home/contact/contact.jsx
+++ src/components/home/contact/contact.jsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import SectionLabel from "../sectionLabel";
 import Button from "../button";
+import * as styles from "../../../styles/home/home.module.css";
+
+const {
+  contact,
+  contactFlex,
+  fieldGroup,
+  inputWrapper,
+  textareaContainer,
+  submitContainer,
+} = styles;
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -16,49 +26,57 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact">
+    <section id="contact" className={contact}>
       <SectionLabel>Drop me a Message</SectionLabel>
       <form onSubmit={submitHandler}>
-        <div>
-          <div>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              onChange={(e) => setName(e.currentTarget.value)}
-              value={name}
-              required
-            />
+        <div className={contactFlex}>
+          <div className={inputWrapper}>
+            <FieldGroup>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                onChange={(e) => setName(e.currentTarget.value)}
+                value={name}
+                required
+              />
+            </FieldGroup>
+            <FieldGroup>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                onChange={(e) => setEmail(e.currentTarget.value)}
+                value={email}
+                required
+              />
+            </FieldGroup>
           </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={(e) => setEmail(e.currentTarget.value)}
-              value={email}
-              required
-            />
+          <div className={inputWrapper}>
+            <FieldGroup className={textareaContainer}>
+              <label htmlFor="message">Message</label>
+              <textarea
+                name="message"
+                id="message"
+                onChange={(e) => setMessage(e.currentTarget.value)}
+                value={message}
+                required
+              />
+            </FieldGroup>
           </div>
         </div>
-        <div>
-          <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            onChange={(e) => setMessage(e.currentTarget.value)}
-            value={message}
-            required
-          />
-        </div>
-        <div>
+        <div className={submitContainer}>
           <Button type="submit">Send Message</Button>
         </div>
       </form>
     </section>
   );
+};
+
+const FieldGroup = ({ children, className }) => {
+  return <div className={`${fieldGroup} ${className}`}>{children}</div>;
 };
 
 export default Contact;
