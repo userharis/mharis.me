0bf58edef07f6f6a7dd97960c69024ef6bb29952
userharis <mhkcodes@gmail.com>
2022-06-23 17:26:59 +0500

    add blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,3 @@
-import "../styles/globals.css";
-import Layout from "../components/home/layout/layout";
-import Hero from "../components/home/hero/hero";
-import Featured from "../components/home/featured/featured";
-import Blog from "../components/home/blog/blog";
-import Contact from "../components/home/contact/contact";
-
 const IndexPage = () => {
   return <h1>home</h1>;
 };
