320fa9d4a790e78fd253a36cc2cda64e1d161ca5
userharis <mhkcodes@gmail.com>
2022-07-13 16:34:38 +0500

    add posts section

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout/layout";
 import Container from "../../components/layout/container";
 import Hero from "../../components/blog/hero";
+import Posts from "../../components/blog/posts";
 import * as styles from "../../styles/pages/global.module.css";
 
 const Blog = () => {
@@ -9,6 +10,7 @@ const Blog = () => {
       <main>
         <Container maxWidth="1040px" className={styles.container}>
           <Hero />
+          <Posts />
         </Container>
       </main>
     </Layout>
